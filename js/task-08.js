d72e48c2f1c3e750cb6e075470d2b9166be93d36
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-07-30 21:13:10 +0300

    0.0.7

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+const elements = {
+    loginForm: document.querySelector(`.login-form`),
+};
 
+
+//console.dir(elements.loginForm.elements);
+//console.dir(elements.loginForm);
+
+elements.loginForm.addEventListener(`submit`, handlerSubmit);
+
+
+function handlerSubmit(evt) {
+    
+    evt.preventDefault();
+    //console.log(evt.currentTarget.elements);
+
+    const {email, password} = evt.currentTarget.elements;
+      
+    console.log(email.value);
+    console.log(password.value);
+       
+    const userCard = {
+        email: evt.currentTarget.email.value,
+        password: evt.currentTarget.password.value,
+        };
+            
+        console.log(`E-mail:`, userCard.email);
+        console.log(`Password:`, userCard.password);
+
+};
+
+elements.loginForm.addEventListener("blur", () => {
+    elements.loginForm.elements.value = "";
+  });
