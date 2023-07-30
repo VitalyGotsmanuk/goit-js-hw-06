3ffdba0a33cd59b7b32fed24fa1718b28bf4491b
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-07-30 21:50:36 +0300

    0.0.9
    no 8

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const butn = document.querySelector(`.change-color`);
+const bodyColor = document.querySelector(`body`);
+const colorValue = document.querySelector(`.color`)
+
+//console.dir(bodyColor);
+// console.log(bodyColor.style.backgroundColor);
+//console.log(colorValue.textContent);
+
+butn.addEventListener(`click`, handlerClick);
+
+function handlerClick (evt){
+  let randmColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randmColor;
+  colorValue.textContent = randmColor;
+};
+
