62107151aad7d601a648b9f850c0f9cfa88378d8
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-08-02 12:26:04 +0300

    2.0

--- js/task-02.js
+++ js/task-02.js
@@ -9,15 +9,20 @@ const ingredients = [
 
 const list = document.querySelector(`#ingredients`);
 
-// const li = document.createElement(`li`);
-
 function createList(arr) {
+  
+  const myList= [];
+  
   arr.forEach(element => {
     const li = document.createElement(`li`);
     li.textContent = [element];
     li.classList.add(`item`);
-    list.append(li);
+    myList.push(li)
   });
+  
+  console.dir(myList);
+  
+  list.append(...myList)
 }
 createList (ingredients);
 
